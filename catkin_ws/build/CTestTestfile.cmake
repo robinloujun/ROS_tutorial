36a8dff217ff57d37078243561d2174345d944d5
Jun Lou <lou@fzi.de>
2018-04-08 13:27:33 +0200

    follow the tutorial creating a workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robin/ROS_tutorial/catkin_ws/src
+# Build directory: /home/robin/ROS_tutorial/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
