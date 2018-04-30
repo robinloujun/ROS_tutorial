46e89bf078949a3de78fa772418ff1bcb5294a63
Jun Lou <lou@fzi.de>
2018-04-30 15:34:31 +0200

    catkin_make

--- catkin_ws/build/beginner_tutorials/catkin_generated/package.cmake
+++ catkin_ws/build/beginner_tutorials/catkin_generated/package.cmake
@@ -1,6 +1,6 @@
 set(_CATKIN_CURRENT_PACKAGE "beginner_tutorials")
 set(beginner_tutorials_VERSION "0.0.0")
-set(beginner_tutorials_MAINTAINER "robin <robin@todo.todo>")
+set(beginner_tutorials_MAINTAINER "Jun Lou <jun.lou@student.kit.edu>")
 set(beginner_tutorials_PACKAGE_FORMAT "2")
 set(beginner_tutorials_BUILD_DEPENDS "roscpp" "rospy" "std_msgs")
 set(beginner_tutorials_BUILD_EXPORT_DEPENDS "roscpp" "rospy" "std_msgs")
